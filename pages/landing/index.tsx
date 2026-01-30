bf99ead217ad29ecc528c04070781c9611b767c2
SOMIE <somie@example.com>
2026-01-30 10:40:41 +0100

    feat(landing): add navbar component with light/dark theme support

--- pages/landing/index.tsx
+++ pages/landing/index.tsx
@@ -2,7 +2,7 @@ import { KeyFeatures } from "@/components/landing/key-features";
 import Hero from "./hero";
 import Footer from "@/components/common/footer";
 // import DashBoard from "@/components/dashboard/dashboard-page"
-import LandingPageNavBar from "@/components/landing/landing-page-nav-bar";
+import LandingPageNavBar from "@/components/landing/navbar";
 import BenefitsSection from "@/components/landing/benefits";
 import ValuePropositions from "@/components/landing/value-propositions";
 import GetStartedCTA from "@/components/landing/get-started-cta";
